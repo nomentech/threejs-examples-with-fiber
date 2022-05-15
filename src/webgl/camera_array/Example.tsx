4088413ea54f35d0d1f69747dfa8e2856718720e
Nomad <nomad@Nomads-MacBook-Pro.local>
2022-05-15 16:50:56 -0400

    camera / array, updated

--- src/webgl/camera_array/Example.tsx
+++ src/webgl/camera_array/Example.tsx
@@ -1,40 +1,33 @@
+import { useEffect, useRef } from "react"
 import { PerspectiveCamera, Vector4 } from "three"
 import { Canvas, useFrame, useThree } from "@react-three/fiber"
-import { useEffect, useRef } from "react"
 
-let CANVAS_WIDTH = window.innerWidth > 640 ? (window.innerWidth - 300) : window.innerWidth
-let CANVAS_HEIGHT = window.innerWidth > 640 ? window.innerHeight : (window.innerHeight - 48)
-const AMOUNT = 6
-const ASPECT_RATIO = CANVAS_WIDTH / CANVAS_HEIGHT
-const WIDTH = (CANVAS_WIDTH / AMOUNT) * window.devicePixelRatio
-const HEIGHT = (CANVAS_HEIGHT / AMOUNT) * window.devicePixelRatio
+import { aspect_ratio, canvas_height, canvas_width } from '../../contants'
+
+const amount = 6
+const width = (canvas_width / amount) * window.devicePixelRatio
+const height = (canvas_height / amount) * window.devicePixelRatio
 let camera: any, renderer: any
 
 const onWindowResize = () => {
-  CANVAS_WIDTH = window.innerWidth > 640 ? (window.innerWidth - 300) : window.innerWidth
-  CANVAS_HEIGHT = window.innerWidth > 640 ? window.innerHeight : (window.innerHeight - 48)
-
-  const ASPECT_RATIO = CANVAS_WIDTH / CANVAS_HEIGHT
-  const WIDTH = (CANVAS_WIDTH / AMOUNT) * window.devicePixelRatio
-  const HEIGHT = (CANVAS_HEIGHT / AMOUNT) * window.devicePixelRatio
-
-  camera.aspect = ASPECT_RATIO
+  camera.aspect = aspect_ratio
   camera.current.updateProjectionMatrix()
 
-  for (let y = 0; y < AMOUNT; y++) {
-    for (let x = 0; x < AMOUNT; x++) {
-      const subcamera = camera.current.cameras[AMOUNT * y + x]
+  for (let y = 0; y < amount; y++) {
+    for (let x = 0; x < amount; x++) {
+      const subcamera = camera.current.cameras[amount * y + x]
       subcamera.viewport.set(
-        Math.floor(x * WIDTH),
-        Math.floor(y * HEIGHT),
-        Math.ceil(WIDTH),
-        Math.ceil(HEIGHT))
+        Math.floor(x * width),
+        Math.floor(y * height),
+        Math.ceil(width),
+        Math.ceil(height))
 
-      subcamera.aspect = ASPECT_RATIO
+      subcamera.aspect = aspect_ratio
       subcamera.updateProjectionMatrix()
     }
   }
-  if (renderer) renderer.setSize(CANVAS_WIDTH, CANVAS_HEIGHT)
+  if (renderer) renderer.setSize(canvas_width
+  , canvas_height)
 }
 
 const Background = () => {
@@ -64,12 +57,12 @@ const Cylinder = () => {
 
 const Cameras = () => {
   const cameras = []
-  for (let y = 0; y < AMOUNT; y ++) {
-    for (let x = 0; x < AMOUNT; x ++) {
-      const subcamera: any = new PerspectiveCamera(40, ASPECT_RATIO, 0.1, 10)
-      subcamera.viewport = new Vector4( Math.floor(x * WIDTH), Math.floor(y * HEIGHT), Math.ceil(WIDTH), Math.ceil(HEIGHT))
-      subcamera.position.x = (x / AMOUNT) - 0.5
-      subcamera.position.y = 0.5 - (y / AMOUNT)
+  for (let y = 0; y < amount; y ++) {
+    for (let x = 0; x < amount; x ++) {
+      const subcamera: any = new PerspectiveCamera(40, aspect_ratio, 0.1, 10)
+      subcamera.viewport = new Vector4( Math.floor(x * width), Math.floor(y * height), Math.ceil(width), Math.ceil(height))
+      subcamera.position.x = (x / amount) - 0.5
+      subcamera.position.y = 0.5 - (y / amount)
       subcamera.position.z = 1.5
       subcamera.position.multiplyScalar(2)
       subcamera.lookAt(0, 0, 0)
@@ -81,7 +74,6 @@ const Cameras = () => {
   camera = useRef()
   const set = useThree((state) => state.set)
   useEffect(() => set({ camera: camera.current }), [])
-  // useFrame(() => camera.current.updateMatrixWorld())
 
   return (
     <arrayCamera ref={camera} cameras={cameras} position={[0, 0, 3]} />
